ff54335de490307570b5f5378e66b88a531628e1
AdorkablyNicole <hello@nicolegeorge.me>
2021-02-27 20:57:23 +1030

    adding constructor function to word scroll elements

---
+++ script.js
@@ -0,0 +1,29 @@
+// building the constructor function
+
+const TypeWriter = function (txtElement, words, wait = 3000) {
+  this.txtElement = txtElement;
+  this.words = words;
+  this.txt = '';
+  this.wordIndex = 0;
+  this.wait = parseInt(wait, 10);
+  this.type();
+  this.isDeleting = false;
+};
+
+// Type Method
+
+// init on DOM load - need event handler
+
+document.addEventListener('DOMContentLoaded', init);
+
+// init app
+
+function init() {
+  const txtElement = document.querySelector('.txt-type');
+  const words = JSON.parse(txtElement.getAttribute('data-words'));
+  const wait = txtElement.getAttribute('data-wait');
+
+  // init TypeWriter
+
+  new TypeWriter(txtElement, words, wait);
+}
